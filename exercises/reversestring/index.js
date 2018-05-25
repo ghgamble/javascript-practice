2724ad7e72ac6d181462a773747c601910005693
Grace Gamble <grace.h.gamble@gmail.com>
2018-05-25 14:23:49 -0600

    Cleaned Up Files

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+      // First Solution
+      // const arr = str.split('');
+      // arr.reverse();
+      // return arr.join('');
+
+      //Second Solution
+      // return str.split('').reverse().join('');
+
+      //Third Solution
+      let reversed = '';
+      for (let character of str) {
+            reversed = character + reversed;
+      }
+      return reversed;
+}
 
 module.exports = reverse;
